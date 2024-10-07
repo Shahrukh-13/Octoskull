ba0890622a474722e30e45ada29ad3809972a169
Shahrukh-13 <m.shahrukh.tariq@gmail.com>
2024-10-07 17:22:47 -0400

    updated code files, added hex files

--- Arduino/Octoskull-Animation_live/Octoskull-Animation_live.ino
+++ Arduino/Octoskull-Animation_live/Octoskull-Animation_live.ino
@@ -1,23 +1,26 @@
 #include <Adafruit_TiCoServo.h>
 #include <Arduino.h>
 
-#define PWM_PIN_1  2
-#define PWM_PIN_2  3
-#define PWM_PIN_3  5
-#define PWM_PIN_4  6
-#define PWM_PIN_5  7
-#define PWM_PIN_6  8
-#define PWM_PIN_7  11
-#define PWM_PIN_8  12
-#define PWM_PIN_9  13
-#define PWM_PIN_10 44
-#define PWM_PIN_11 45
-#define PWM_PIN_12 46
+// Servo pins
+#define Servo_PIN_1  2
+#define Servo_PIN_2  3
+#define Servo_PIN_3  5
+#define Servo_PIN_4  6
+#define Servo_PIN_5  7
+#define Servo_PIN_6  8
+#define Servo_PIN_7  11
+#define Servo_PIN_8  12
+#define Servo_PIN_9  13
+#define Servo_PIN_10 44
+#define Servo_PIN_11 45
+#define Servo_PIN_12 46
 
+// Relay GPIO Pins
 #define RELAY_PIN_1 22
 #define RELAY_PIN_2 23
 #define RELAY_PIN_3 24
 
+// Servo Objects
 Adafruit_TiCoServo servo1;   
 Adafruit_TiCoServo servo2;
 Adafruit_TiCoServo servo3;
@@ -32,38 +35,44 @@ Adafruit_TiCoServo servo11;
 Adafruit_TiCoServo servo12;
 
 
-uint8_t data;     // Varriabel for reading incoming byte over serial port 
-uint8_t res[5]; // Character array for storing read bytes
-uint8_t i;         // Varriable for incrementing char array index
+uint8_t data;     // for reading incoming byte over serial port 
+uint8_t res[5];  // Character array for storing read bytes
+uint8_t i;       // for incrementing char array index
+
 uint8_t servo_angle;
 
 
 void setup() 
 {
+  // Init Serial
   Serial.begin(115200);
   Serial.flush();
-  
+
+  // set Relay GPIOs as output
   pinMode(RELAY_PIN_1, OUTPUT);
   pinMode(RELAY_PIN_2, OUTPUT);
   pinMode(RELAY_PIN_3, OUTPUT);
 
+  // keep the relays disabled
   digitalWrite(RELAY_PIN_1, LOW);
   digitalWrite(RELAY_PIN_2, LOW);
   digitalWrite(RELAY_PIN_3, LOW);
-  
-  servo1.attach(PWM_PIN_1);
-  servo2.attach(PWM_PIN_2);
-  servo3.attach(PWM_PIN_3);
-  servo4.attach(PWM_PIN_4);
-  servo5.attach(PWM_PIN_5);
-  servo6.attach(PWM_PIN_6);
-  servo7.attach(PWM_PIN_7);
-  servo8.attach(PWM_PIN_8);
-  servo9.attach(PWM_PIN_9);
-  servo10.attach(PWM_PIN_10);
-  servo11.attach(PWM_PIN_11);
-  servo12.attach(PWM_PIN_12);
 
+  // Init Servos
+  servo1.attach(Servo_PIN_1);
+  servo2.attach(Servo_PIN_2);
+  servo3.attach(Servo_PIN_3);
+  servo4.attach(Servo_PIN_4);
+  servo5.attach(Servo_PIN_5);
+  servo6.attach(Servo_PIN_6);
+  servo7.attach(Servo_PIN_7);
+  servo8.attach(Servo_PIN_8);
+  servo9.attach(Servo_PIN_9);
+  servo10.attach(Servo_PIN_10);
+  servo11.attach(Servo_PIN_11);
+  servo12.attach(Servo_PIN_12);
+
+  // Write Initial Servo Positions
   servo1.write(55);
   servo2.write(24);
   servo3.write(120);
@@ -77,28 +86,18 @@ void setup()
   servo11.write(90);
   servo12.write(90);
  
-
+  // Enable the relays after writting servo positions
   digitalWrite(RELAY_PIN_1, HIGH);
   digitalWrite(RELAY_PIN_2, HIGH);
   digitalWrite(RELAY_PIN_3, HIGH);
 
-  delay(2000);
+  delay(2000); //The delay is here so that  the octoskull can settle down in its initial pose before it starts reading the new servo positions over serial port from blender 
 }
 
 void loop() 
-{
-  i=0;   
-  while( data!= 0x3E)     
-  {
-    while(Serial.available())
-    {
-      data=Serial.read();
-      res[i]=data;
-      i++;
-    }
-  }
-  data=0;
-
+{  
+  Serial_Receive();
+  
   if(res[0]==0x3C && res[4]==0x3E)
   {
     servo_angle= res[3];
@@ -151,4 +150,24 @@ void loop()
       servo12.write(servo_angle);
     }
   }
+  
+  else
+  {
+    //DO NOTHING
+  }
+}
+
+void Serial_Receive()
+{
+  i=0;   
+  while( data!= 0x3E)     
+  {
+    while(Serial.available())
+    {
+      data=Serial.read();
+      res[i]=data;
+      i++;
+    }
+  }
+  data=0;
 }
